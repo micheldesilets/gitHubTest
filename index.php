cf7d7ea20bcaf237fd4484342fbf24f265f37654
Michel Desilets <desilets.michel.2007@gmail.com>
2018-07-08 12:49:55 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+$users = array(
+  (object)array('username' => 'Michel', 'password' => 'secret'),
+  (object)array('username' => 'Luc', 'password' => 'spaniard'),
+  (object)array('username' => 'Pierre', 'password' => 'secret'),
+);
+
+function matches($a, $b)
+{
+  return $a == $b;
+}
+
+function authenticate($users, $username, $password)
+{
+  for ($i = 0; $i < count($users); $i++) {
+    if (matches($users[$i]->username, $username) && matches($users[$i]->password, $password)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+$isAuthenticated = authenticate($users, 'Luc', 'spniard');
+if ($isAuthenticated) {
+  echo 'Welcome, known user!';
+} else {
+  echo 'Not authenticated.';
+}
